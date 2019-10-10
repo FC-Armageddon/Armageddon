30ec55798b21e1a74e0cf58e3ba83def1efebc28
7painapo7 <spurs11714@gmail.com>
2019-10-10 07:30:50 +0000

    [Add]userのshow,edit,update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
+    redirect_to user_path(@user.id)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def deleted_flag
@@ -24,5 +30,10 @@ class UsersController < ApplicationController
   end
 
   def admins_deleted_flag
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :email)
   end
 end
