8faf79cf0c8b44475df5d1fc96830c66ce5e2fc5
daiki0809 <cookietanaka0809@gmail.com>
2019-10-15 06:43:38 +0000

    [Finish]販売ステータス

--- app/controllers/arrivals_controller.rb
+++ app/controllers/arrivals_controller.rb
@@ -1,12 +1,28 @@
 class ArrivalsController < ApplicationController
-  def create
+def create
   	arrival = Arrival.new(arrival_params)
-  	arrival.save
+  	arrival.save!
   	cd = Cd.find(arrival.cd_id)
+  if cd.sales_status.sales_status == "販売中"
   	cd.stock = cd.stock + arrival.arrival
-  	cd.save
-  	redirect_to cd_admins_path(cd.id)
+    if cd.stock <= 0
+      sales = SalesStatus.find_by(sales_status: "販売停止中")
+      cd.sales_status_id = sales.id
+    end
+  else
+    if cd.stock <= 0
+      cd.stock = cd.stock + arrival.arrival
+      if cd.stock >= 1
+        sales = SalesStatus.find_by(sales_status: "販売中")
+        cd.sales_status_id = sales.id
+      end
+    else
+      cd.stock = cd.stock + arrival.arrival
+    end
   end
+    cd.save!
+    redirect_to cd_admins_path(cd.id)
+end
 
   def index
     @arrivals = Arrival.all
