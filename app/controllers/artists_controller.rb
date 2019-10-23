a68876080821917475094c41ad5f77789a7d0e94
daiki0809 <cookietanaka0809@gmail.com>
2019-10-23 11:37:28 +0000

    [Delete]不要なコントローラ

--- app/controllers/artists_controller.rb
+++
@@ -1,7 +0,0 @@
-class ArtistsController < ApplicationController
-  def create
-  end
-
-  def update
-  end
-end
--- app/controllers/sales_statuses_controller.rb
+++
@@ -1,4 +0,0 @@
-class SalesStatusesController < ApplicationController
-  def create
-
-end
