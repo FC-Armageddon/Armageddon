13a553d790743ac81daef8a198864d8f99e7396c
daiki0809 <cookietanaka0809@gmail.com>
2019-10-24 03:25:16 +0000

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_22_043117) do
+ActiveRecord::Schema.define(version: 2019_10_23_120754) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -93,6 +93,13 @@ ActiveRecord::Schema.define(version: 2019_10_22_043117) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "cd_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "genres", force: :cascade do |t|
     t.text "genre"
     t.datetime "created_at", null: false
@@ -112,6 +119,14 @@ ActiveRecord::Schema.define(version: 2019_10_22_043117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "cd_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sales_statuses", force: :cascade do |t|
