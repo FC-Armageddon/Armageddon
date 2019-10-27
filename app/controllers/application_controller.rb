866f2c6e189c086091d17205a520b1a7af6b424b
daiki0809 <cookietanaka0809@gmail.com>
2019-10-27 05:21:56 +0000

    [Finish]非同期

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
 	  before_action :configure_permitted_parameters,  if: :devise_controller?
 	  before_action :store_user_location!, if: :storable_location?
-
+    before_action :carts_count
 	  def after_sign_in_path_for(resource_or_scope)
 		stored_location_for(resource_or_scope) || root_path
 	  end
+
+    def carts_count
+      if user_signed_in?
+        user_carts = current_user.carts
+        carts = user_carts.where(deleted_flag: "false")
+        if carts != []
+          @carts_count = carts.count
+        end
+      end
+    end
 
   protected
   def configure_permitted_parameters
