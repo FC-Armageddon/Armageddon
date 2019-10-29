bcf57f21b4b7dd485acf910a10bd856b8da23fce
daiki0809 <cookietanaka0809@gmail.com>
2019-10-29 07:16:26 +0000

    [Add]非同期

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
 
-	before_action :authenticate_user!
+	before_action :sign_in?
 
 	def index
 	end
@@ -10,22 +10,29 @@ class ReviewsController < ApplicationController
 		review = current_user.reviews.new(review_params)
 		review.cd_id = cd.id
         if review.save
-        redirect_to cd_path(cd.id)
     	else
     		flash[:notice] = "レビューは空で投稿できません。"
     		redirect_to cd_path(cd.id)
     	end
+    	@reviews = cd.reviews
 	end
 
 	def destroy
 		review = Review.find(params[:cd_id])
 		cd = review.cd
 		review.delete
-		redirect_to cd_path(cd.id)
+		@reviews = cd.reviews
 	end
 
     private
     def review_params
     	params.require(:review).permit(:user_id, :cd_id, :body)
+    end
+
+    def sign_in?
+    	if user_signed_in?
+    	else
+    		redirect_to new_user_session_path
+    	end
     end
 end
